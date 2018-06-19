c8eb936c15e91d91d8fdc009ede0294d12370fef
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-06-19 15:51:26 -0400

    Remove Turbolinks.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resource :session, only: [:create, :destroy]
   resources :saved_messages, only: [:create, :destroy]
   resources :messages, only: [:create, :update]
-  get '/channels' => 'channels#index'
-  post '/channels/:id/join' => 'channels#join'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/channels', to: 'channels#index'
+  post '/channels/:id/join', to: 'channels#join'
 end
